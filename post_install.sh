9de956bfcd391385ac547afafce14c4f3cfa3265
Subak Systems <subak@rmbp13.local>
2016-05-08 17:06:14 +0900

    日  5  8 17:06:14 JST 2016

--- post_install.sh
+++ post_install.sh
@@ -10,3 +10,8 @@ ln -s ${HOME}/Repos/13.rmbp/.profile ${HOME}/.profile
 cd ${HOME}/Library
 git clone https://github.com/yascentur/RictyDiminished Fonts
 
+
+karabiner=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+$karabiner append "HHKB on MBP"
+$karabiner select_by_name "HHKB on MBP"
+${HOME}/Repos/dotfiles/hhkb_on_mbp.sh
