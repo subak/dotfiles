1f53d806c7ae1139dcb3390bf65d48f4b230a2de
Subak Systems <subak@rmbp13.local>
2016-05-08 16:43:56 +0900

    日  5  8 16:43:56 JST 2016

--- install.sh
+++ install.sh
@@ -1,14 +1,68 @@
 #!/usr/bin/env bash
 
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install caskroom/cask/brew-cask
+set -eu
 
-install_brew_packages.sh
-install_brew_cask_packages.sh
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-gem install pry
-gem install asciidoctor coderay rouge pygments.rb
-gem install asciidoctor-pdf --pre
+brew install \
+  caskroom/cask/brew-cask \
+  bash-completion \
+  homebrew/dupes/screen \
+  stunnel
 
+
+# unison
+brew install \
+  unison \
+  python3
+pip3 install macfsevents
+
+
+brew cask install \
+  google-chrome \
+  karabiner \
+  seil \
+  skitch \
+  textmate \
+  near-lock \
+  sshfs \
+  sequel-pro \
+  teamviewer \
+  dockertoolbox \
+  duet \
+  intellij-idea \
+  evernote \
+  dropbox \
+  1password \
+  chatwork \
+  skype \
+  slack \
+  sketch
+
+
+# KeyBindings
+mkdir -pv ${HOME}/Library/KeyBindings
+ln -s ${HOME}/Repos/dotfiles/DefaultKeyBinding.dict ${HOME}/Library/KeyBindings/
+ln -s ${HOME}/Repos/dotfiles/DefaultKeyBinding.dict "${HOME}/Library/Application Support/Textmate/KeyBindings.dict"
+
+
+# Karabiner
+mkdir -pv ${HOME}/Library/Application\ Support/Karabiner
+ln -s ${HOME}/Repos/dotfiles/private.xml "${HOME}/Library/Application Support/Karabiner/"
+
+
+# IntteliJIDEA
+IDEA_VERSION=15
+mkdir -pv ${HOME}/Library/Preferences/IntelliJIdea${IDEA_VERSION}
+ln -s ${HOME}/Repos/dotfiles/idea/codestyles ${HOME}/Library/Preferences/IntelliJIdea${IDEA_VERSION}/
+ln -s ${HOME}/Repos/dotfiles/idea/colors ${HOME}/Library/Preferences/IntelliJIdea${IDEA_VERSION}/
+ln -s ${HOME}/Repos/dotfiles/idea/keymaps ${HOME}/Library/Preferences/IntelliJIdea${IDEA_VERSION}/
+
+
+# Textmate QuickLook Plugin
+cp -a ${HOME}/Applications/TextMate.app/Contents/Library/QuickLook ${HOME}/Library/
+
+
+# Git
 git config --global user.email info@subak.jp
 git config --global core.editor nano
---
+++ post_install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# sshkeyの設定後
+
+cd ${HOME}/Repos
+git clone git@bitbucket.org:subak/13.rmbp.git
+ln -s ${HOME}/Repos/13.rmbp/.profile ${HOME}/.profile
+
+
+cd ${HOME}/Library
+git clone https://github.com/yascentur/RictyDiminished Fonts
+
