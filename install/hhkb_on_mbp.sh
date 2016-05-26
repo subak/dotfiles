dc1d417fde4e0a1456b3067a03896014ed4d32d8
高橋寛行 <info@subak.jp>
2016-05-26 10:28:15 +0900

    2016年 5月26日 木曜日 10時28分15秒 JST

--- install/hhkb_on_mbp.sh
+++ install/hhkb_on_mbp.sh
@@ -2,27 +2,25 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
-$cli set remap.fn_fkeys_to_consumer_f5 1
-/bin/echo -n .
 $cli set repeat.initial_wait 250
 /bin/echo -n .
-$cli set remap.pc_application2fn 1
+$cli set remap.jis_eisuu2commandL_eisuu 1
 /bin/echo -n .
 $cli set remap.jis_yen2backslash 1
 /bin/echo -n .
-$cli set remap.jis_eisuu2commandL_eisuu 1
+$cli set remap.pc_application2fn 1
 /bin/echo -n .
 $cli set option.emacsmode_controlM 1
 /bin/echo -n .
 $cli set remap.enter2delete 1
 /bin/echo -n .
-$cli set repeat.wait 81
+$cli set remap.mouse_keys_mode_mbp 1
+/bin/echo -n .
+$cli set repeat.wait 33
 /bin/echo -n .
 $cli set remap.jis_jansi 1
 /bin/echo -n .
 $cli set jp.subak.remap.emacsmode_controlP 1
-/bin/echo -n .
-$cli set remap.mouse_keys_mode_mbp 1
 /bin/echo -n .
 $cli set jp.subak.remap.windows_option_tab_to_command_tab 1
 /bin/echo -n .
@@ -34,28 +32,28 @@ $cli set remap.net_tk84_backquote_to_shift_r 1
 /bin/echo -n .
 $cli set remap.fn_fkeys_to_consumer_f3_lion 1
 /bin/echo -n .
-$cli set option.emacsmode_controlPNBF_nomodifiers 1
-/bin/echo -n .
 $cli set option.emacsmode_optionV 1
 /bin/echo -n .
-$cli set jp.subak.remap.emacsmode_controlH 1
-/bin/echo -n .
-$cli set jp.subak.remap.emacsmode_controlLeftbracket 1
+$cli set option.emacsmode_controlPNBF_nomodifiers 1
 /bin/echo -n .
 $cli set private.remotedesktopswapbackquote 1
 /bin/echo -n .
-$cli set remap.mouse_keys_mode_subak 1
+$cli set jp.subak.remap.emacsmode_controlLeftbracket 1
+/bin/echo -n .
+$cli set jp.subak.remap.emacsmode_controlH 1
 /bin/echo -n .
 $cli set remap.fn_fkeys_to_consumer_f1 1
-/bin/echo -n .
-$cli set remap.jis_kana2commandR_kana 1
 /bin/echo -n .
 $cli set remap.fn2optionL 1
 /bin/echo -n .
 $cli set remap.delete2backquote 1
 /bin/echo -n .
-$cli set remap.fn_fkeys_to_consumer_f10 1
+$cli set remap.jis_kana2commandR_kana 1
 /bin/echo -n .
 $cli set remap.fn_fkeys_to_consumer_f7 1
+/bin/echo -n .
+$cli set remap.fn_fkeys_to_consumer_f5 1
+/bin/echo -n .
+$cli set remap.fn_fkeys_to_consumer_f10 1
 /bin/echo -n .
 /bin/echo
