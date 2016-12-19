629f0a499ddb902380bfba4e16a6d3f3f9ef9a23
Subak Systems <info@subak.jp>
2016-12-19 22:54:16 +0900

    2016年 12月19日 月曜日 22時54分16秒 JST

--- install/ssh_for_local.sh
+++ install/ssh_for_local.sh
@@ -7,6 +7,7 @@ certs_dir=${HOME}/Repos/certs
 ssh_dir=${HOME}/.ssh
 key=${certs_dir}/client/${client}.key
 pub=${certs_dir}/client/${client}.pkcs8
+cert=${certs_dir}/client/${client}.pem
 
 git clone git://pro7core/certs.git ${certs_dir}
 mkdir ${ssh_dir}
@@ -14,6 +15,7 @@ chmod 700 ${ssh_dir}
 chmod 600 ${key}
 ln -s ${key} ${ssh_dir}/id_rsa
 ln -s ${pub} ${ssh_dir}/id_rsa.pub
+ln -s ${cert} ${ssh_dir}/cert.pem
 
 cd ${certs_dir}
 git remote rm origin
